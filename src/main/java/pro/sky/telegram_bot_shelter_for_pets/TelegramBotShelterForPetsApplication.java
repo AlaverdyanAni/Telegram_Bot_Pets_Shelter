fea4aad6d078db75d689654625dde2d004a6607b
Ani <ani-alaverdyan@mail.ru>
2023-02-18 17:50:39 +0300

    Initial commit

---
+++ src/main/java/pro/sky/telegram_bot_shelter_for_pets/TelegramBotShelterForPetsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.telegram_bot_shelter_for_pets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotShelterForPetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotShelterForPetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/telegram_bot_shelter_for_pets/TelegramBotShelterForPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.telegram_bot_shelter_for_pets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotShelterForPetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
